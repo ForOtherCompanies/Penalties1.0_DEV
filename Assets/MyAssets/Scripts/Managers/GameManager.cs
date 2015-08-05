0644049ec4b0cb173c6a61ebac85b3ae7896ee0d
lothvorn <lothvorn@gmail.com>
2015-08-05 21:27:24 +0200

    Logica diseñada
    el input es inputManager(lee la entrada de la
    touch)->gameManager(decideque accion realizar a partir de la entrada Y
    calcula lo necesario con los parametros que llegaron del input)->lo que
    corresponda (de momoento pelota.lanzar

---
+++ Assets/MyAssets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	//aqui van variables para saber el estado del juego (p.ej. si estamos en modo portero o tirador... y para
+	//tambien es responasbildad de este manager alternar entre ellos. De momento solo tenemos el tiro a puerta
+
+
+	//keepPublic
+	public GameObject pelota;
+
+	//setPrivate
+	//// variable para el lanzamiento
+	public Vector3 direccionTiro;
+	public Vector3 fuerzaTiro;
+
+
+
+	//only for debug
+	public GameObject raycastMark;
+
+
+
+	public void RealizarAcciones(Vector2 inicioTouch, Vector3 destinoTouch){
+		//if 'estamos como delantero y todo esta correcto para lanzar'
+		////then pelota.fisicas.Lanzar (inicio, fin,fuerza);
+		PrepararLanzamiento (inicioTouch, destinoTouch);
+
+
+	}
+
+
+	void PrepararLanzamiento (Vector2 inicio, Vector2 fin){
+		//raycast y calculo de direccionTiro y fuerzaTiro
+	}
+
+
+
+}
---
+++ Assets/MyAssets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+
+	//to set private
+	public Vector2 inicioTouch = Vector2.zero;
+	public Vector2 finTouch = Vector2.zero;
+
+	Touch myTouch; 
+
+	//references to other scripts
+	public GameManager gameManager;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		if (Input.touchCount>0){
+			myTouch = Input.GetTouch (0);
+			//al inicio del touch guardar la posicion
+			if (myTouch.phase ==TouchPhase.Began){
+				inicioTouch = myTouch.position;
+
+			}
+
+			//al final del touch guaradr la posicion y llamar a la funcion que calcula el tiro pasando los parametros
+			if (myTouch.phase == TouchPhase.Ended){
+				finTouch = myTouch.position;
+				gameManager.RealizarAcciones (inicioTouch, finTouch);			
+			}
+		}
+
+	}
+
+}
