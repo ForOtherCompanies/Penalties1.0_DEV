1f17923cd4e5e0f654dbaa8d8102be81988ebecd
lothvorn <lothvorn@gmail.com>
2015-08-11 19:25:41 +0200

    Efecto FadeCamara listo
    El script de control cuelga del GameController

--- Assets/MyAssets/Scripts/Debug/InputForDebug.cs
+++ Assets/MyAssets/Scripts/Debug/InputForDebug.cs
@@ -9,8 +9,10 @@ public class InputForDebug : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		/*
 		if(Input.GetKeyDown("space")){
 			iaTiro.Tirar();
 		}
+		*/
 	}
 }
---
+++ Assets/MyAssets/Scripts/Efectos/CameraEffects.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEffects : MonoBehaviour {
+
+	//keep public
+	public Animator fadeCameraAnimator;
+/*
+	void Update () {
+
+		if(Input.GetKeyDown("space")){
+			if (fadeCameraAnimator.GetBool ("FadeIn"))
+				fadeCameraAnimator.SetBool ("FadeIn", false);
+			else
+				fadeCameraAnimator.SetBool ("FadeIn", true);
+
+
+			if (fadeCameraAnimator.GetBool ("FadeOut"))
+				fadeCameraAnimator.SetBool ("FadeOut", false);
+			else
+				fadeCameraAnimator.SetBool ("FadeOut", true);
+		}
+
+	}
+*/
+	public void FadeIn () {
+		fadeCameraAnimator.SetBool ("FadeIn", true);
+		fadeCameraAnimator.SetBool ("FadeOut", false);
+	}
+
+	public void FadeOut () {
+		fadeCameraAnimator.SetBool ("FadeIn", false);
+		fadeCameraAnimator.SetBool ("FadeOut", true);
+	}
+
+}
