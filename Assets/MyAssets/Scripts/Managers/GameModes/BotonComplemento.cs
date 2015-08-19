60c3fd27257470d0b0a9a2e1011d7a605a128e86
lothvorn <lothvorn@gmail.com>
2015-08-19 22:15:26 +0200

    Creados prefabs parte GUI

--- Assets/MyAssets/Scripts/Managers/GameModes/BotonComplemento.cs
+++ Assets/MyAssets/Scripts/Managers/GameModes/BotonComplemento.cs
@@ -6,9 +6,15 @@ public class BotonComplemento : MonoBehaviour {
 	//keep public
 	public GameObject complementoAsociado; //puntero al prefab del complemento asociado del boton (necesario para instanciar y colocar cuando se pulse)
 	public CategoriaComplemento categoria; //referencia a la categoria a la que pertenece este boton (y, por tanto, la forma de encontrar el helper donde colocar el objeto)
+	public Texture texturaDisponible;
+	public Texture texturaAunNoComprado;
+
+	//not sure
+	public string nombre;
+	public int precioDinero;
+	public int precioCoins;
+
 	//para comprobar si el objeto esta ya comprado o no
-
-
 	//set private
 	//para comprobar que hacer si se pulsa el boton (si esta comprado 'ponerlo' y si no pues ir a las opciones de compr
 	public bool comprado = false;
@@ -16,7 +22,7 @@ public class BotonComplemento : MonoBehaviour {
 
 
 
-
+	//esta es la funcion a llamar en el onclick sobre el boton
 	public void PerformActions (){
 		if (comprado && !yaPuesto){
 			Destroy (categoria.complementoActivo);
