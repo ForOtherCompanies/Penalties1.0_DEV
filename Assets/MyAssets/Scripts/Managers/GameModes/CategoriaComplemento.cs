dbfc4e45263f45d27132d3c544f5a63f25ed6a1b
lothvorn <lothvorn@gmail.com>
2015-08-22 21:41:59 +0200

    Instanciando botones
    Ahora los botones de los objetos a equipar se instancian desde el script
    de la categoria correspondiente y lo hacen correctamente (falta arreglar
    el reescalado, el paso de linea y el tamaño de marco para el scroll)

--- Assets/MyAssets/Scripts/Managers/GameModes/CategoriaComplemento.cs
+++ Assets/MyAssets/Scripts/Managers/GameModes/CategoriaComplemento.cs
@@ -8,9 +8,29 @@ public class CategoriaComplemento : MonoBehaviour {
 
 	public GameObject boneParent;
 	public GameObject positionHelper;
+
+	public GameObject buttonsHolder;
+
+	public GameObject [] buttonsHelpers;
 	public GameObject complementoActivo;//para mantener en cada categoria apuntado su complemento si es que esta puesto
-	//public arraylist de botones-objeto (cada boton podria ser un 'prefab' que ya incluya el modelo y todo; el modelo estaria desactivado hasta que se seleccione y se coloque en su sitio (esto ya no es necesario)
+	public GameObject [] buttons; //public arraylist de botones-objeto (cada boton podria ser un 'prefab' que ya incluya el modelo y todo; el modelo estaria desactivado hasta que se seleccione y se coloque en su sitio (esto ya no es necesario)
 	////cada prefab tipo 'complemento' debe colgar de un holder para mover el holder a la posicion del helper y luego recolocar el objeto en su
 	////sitio desde unity. OJO con la normalizacion de posiciones que puede ser un jaleo
 
+
+
+	public void Start (){
+
+		for (int i = 0; i < buttonsHelpers.Length; i++){
+			//Debug.Log ("instancio " + button.name);
+			GameObject newButton = Instantiate (buttons[i]);
+			newButton.transform.SetParent(buttonsHolder.transform, false);
+			newButton.transform.position = buttonsHelpers[i%buttonsHelpers.Length].transform.position;
+		}
+		//primero se instancian todos los botones y se colocan en su sitio y tal.
+
+
+		////consultar a la base de datos los objetos disponibles para este jugador y los que sean button.categoria == this.nombre se marcan como
+		////activos. Al pulsarse un boton inactivo este decide si ativarse o no previa consulta a la base de datos y tal
+	}
 }
