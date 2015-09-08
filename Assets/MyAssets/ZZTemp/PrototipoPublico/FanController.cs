481ac7d33acd453d364f6640bc3dc65075b2c2a4
lothvorn <lothvorn@gmail.com>
2015-09-09 00:32:07 +0200

    Prototipo v01 listo
    Ver escena 'Prototipo-Publico'

---
+++ Assets/MyAssets/ZZTemp/PrototipoPublico/FanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanController : MonoBehaviour {
+
+
+
+	Animator animator;
+
+	void Start () {
+
+		animator = GetComponent<Animator>();
+		float startPoint = Random.Range(0f, 1f);
+
+		animator.Play ("FanSTDAnim",-1,startPoint);
+
+
+	}
+
+
+	
+
+}
